53facdbaa6e2e5dc1ec37c0bea714ce3860573c4
TMA\vtmnhung <vtmn1212@gmail.com>
2021-02-24 15:40:51 +0700

    Set up primeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {CalendarModule} from 'primeng/calendar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
