33d9c4ebe393a44f2a6f4bb2ffcd9ea7e1c80311
Dios Vo <vtmn1212@gmail.com>
2021-02-25 17:44:59 +0700

    Create item detail animation

--- src/app/web/budget/list-item/item-card/item-card.component.ts
+++ src/app/web/budget/list-item/item-card/item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-card.component.scss']
 })
 export class ItemCardComponent implements OnInit {
+
+  @Input() isIncome?: boolean = true;
 
   constructor() { }
 
