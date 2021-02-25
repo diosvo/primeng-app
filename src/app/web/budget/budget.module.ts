8175fbf93caed74bb453a2056cbd9a700c6fb53b
Dios Vo <vtmn1212@gmail.com>
2021-02-25 15:52:53 +0700

    Update global styles

--- src/app/web/budget/budget.module.ts
+++ src/app/web/budget/budget.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule } from '@angular/forms';
+
 import { BudgetRoutingModule } from './budget-routing.module';
 import { BudgetComponent } from './budget.component';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [BudgetComponent],
